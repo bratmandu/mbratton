38dd5d4971a121bec0cc2a4537bf092f2bb78901
bratmandu <michaeljamesbratton@googlemail.com>
2022-09-02 15:25:50 +0100

    005 - adding fetch call on load

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {
+  useState, useEffect
+} from 'react'
 import MyTable from '../table/myTable'
 import MyHeader from '../header/myHeader'
 import './main.scss'
 
 function Main() {
+  const [tableData, setTableData] = useState([])
+
+  const myUrl = 'https://run.mocky.io/v3/198ec2ca-fe61-46d4-b0a6-b2067452c0b1'
+
+  useEffect(() => {
+    console.log('I am use effect hook!')
+    fetch(myUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data: ', data)
+      })
+  })
+
   return (
     <div>
       <MyHeader
