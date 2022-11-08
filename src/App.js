cf93b3326b2aef5019b0f46f89951d9bb91d169a
bratmandu <michaeljamesbratton@googlemail.com>
2022-11-08 11:29:03 +0000

    009 - update routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import { Routes, Route, BrowserRouter as Router, Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import {
+  Routes, Route, Link, useLocation
+} from 'react-router-dom'
 import Main from './components/main/main'
 import Second from './components/second/second'
 
 function App() {
+  const location = useLocation()
+
+  useEffect(() => {
+    console.log('location: ', location)
+  }, [location])
+
   return (
     <div>
       <div>Status text</div>
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">Employee Table</Link>
-          </li>
-          <li>
-            <Link to="/secondary">Information</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/secondary" element={<Second />} />
-        </Routes>
-      </Router>
+      <ul>
+        <li>
+          <Link to="/">Employee Table</Link>
+        </li>
+        <li>
+          <Link to="/secondary">Information</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/secondary" element={<Second />} />
+        <Route path="*" element={<Main />} />
+      </Routes>
     </div>
   )
 }
--- src/components/table/myTable.js
+++ src/components/table/myTable.js
@@ -1,6 +1,7 @@
 import React, {
   useState, useEffect
 } from 'react'
+import {v4 as uuid} from 'uuid'
 import './table.scss'
 
 function MyTable() {
@@ -32,16 +33,11 @@ function MyTable() {
   })
 
   const removeRow = (rowData) => {
-    console.log('remove row: ', rowData)
-    console.log('filter: ', mainData.filter((row) => (row.id !== rowData.id)))
     setMainData(mainData.filter((row) => (row.id !== rowData.id)))
   }
 
   const updateRow = (value, rowData, field) => {
     const rowToUpdate = mainData.filter((row) => (row.id === rowData.id))
-    console.log('value: ', value)
-    console.log('rowToUpdate[0]: ', rowToUpdate[0])
-    console.log('field: ', field)
     rowToUpdate[0][field] = value
   }
 
@@ -50,7 +46,7 @@ function MyTable() {
       <thead>
         <tr>
           {headerCols.map((col) => (
-            <td>
+            <td key={uuid()}>
               {col}
             </td>
           ))}
@@ -58,9 +54,10 @@ function MyTable() {
       </thead>
       <tbody>
         {mainData.map((data) => (
-          <tr key={data.id}>
+          <tr key={uuid()}>
             {Object.entries(data).map(([prop, value]) => (
               <td
+                key={uuid()}
                 contentEditable={data.id === editingRow}
                 field={prop}
                 onBlur={(event) => {
@@ -70,7 +67,7 @@ function MyTable() {
                 {value}
               </td>
             ))}
-            <td>
+            <td key={uuid()}>
               <button type="button" onClick={() => { removeRow(data) }}>
                 Delete Row
               </button>
