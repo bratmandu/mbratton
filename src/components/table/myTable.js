13bc1f618e5c8b8b2fe1c4cb6a1a4e82b3223c5f
bratmandu <michaeljamesbratton@googlemail.com>
2022-11-14 10:10:53 +0000

    013 - remove uuid import

--- src/components/table/myTable.js
+++ src/components/table/myTable.js
@@ -1,7 +1,6 @@
 import React, {
   useState, useEffect
 } from 'react'
-import { v4 as uuid } from 'uuid'
 import './table.scss'
 import { AgGridReact } from 'ag-grid-react'
 import colDefs from './myTableColDefs'
